19fbbfa409ccc1d286ecc2dfb0a53ba6f7891871
TanogT <gaetanotorrisi18@gmail.com>
2022-11-21 22:01:12 +0100

    tuttoOKdaPulire

--- Jni.java
+++ Jni.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Jni {
 	
 	static {
@@ -8,11 +6,4 @@ public class Jni {
 	
 	public native int[] getMossaBot(int liv, int mat[][], int nK, int nF);	//rig - col - nK
 	public native String stampami();
-	
-	public static void main(String[] args) {
-		Jni c = new Jni();
-		
-		System.out.println(Arrays.toString(c.getMossaBot(3, MatAlg.mat, 0, 10)));
-		
-	}
 }
